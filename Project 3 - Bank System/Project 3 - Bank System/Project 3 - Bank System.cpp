38cfb496f8ba70e6c507da77d15b13feecba9612
Mustafa Najeh <mustafapvp250@gmail.com>
2026-06-24 16:55:33 +0300

    Prepare Currency Object

--- Project 3 - Bank System/Project 3 - Bank System/Project 3 - Bank System.cpp
+++ Project 3 - Bank System/Project 3 - Bank System/Project 3 - Bank System.cpp
@@ -2,16 +2,40 @@
 #include "clsMainMenueScreen.h"
 #include "clsLoginScreen.h"
 #include "clsUser.h"
+#include "clsCurrency.h"
 using namespace std;
 
+static void _PrintOneCurrency(clsCurrency Currency) {
+	cout << "\n_____________________________\n";
+	cout << "Countrey      : " << Currency.Country() << "\n";
+	cout << "Currency Code : " << Currency.CurrencyCode() << "\n";
+	cout << "Currency Name : " << Currency.CurrencyName() << "\n";
+	cout << "Rate          : " << Currency.Rate();
+	cout << "\n_____________________________\n";
+
+}
+
 int main() {
-	while (true) {
+
+	
+	clsCurrency Currency1 =	clsCurrency::FindCountry("iraq");
+	_PrintOneCurrency(Currency1);
+
+	clsCurrency Currency2 = clsCurrency::FindCurrencyCode("irr");
+	_PrintOneCurrency(Currency2);
+
+	Currency1.UpdateRate(1600);
+	_PrintOneCurrency(Currency1);
+
+
+
+	/*while (true) {
 
 		if (!clsLoginScreen::ShowLoginScreen()) {
 			break;
 		}
 
-	}
+	}*/
 	system("pause>0");
 
 }
---
+++ Project 3 - Bank System/Project 3 - Bank System/clsCurrency.h
@@ -0,0 +1,213 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include "clsString.h"
+#include <fstream>
+using namespace std;
+
+class clsCurrency
+{
+private:
+
+	enum enMode {
+		EmpytMode = 1, UpdateMode = 2
+	};
+	enMode _Mode;
+
+	string _Country;
+	string _CurrencyCode;
+	string _CurrencyName;
+	float _Rate;
+	
+	static clsCurrency _GetEmptyCurrencyObject(){
+		return clsCurrency(enMode::EmpytMode, "", "", "", 0);
+	}
+
+	static string _ConvertCurrencyRecordToLine(clsCurrency Currency, string delim = "//##//") {
+
+		string sRecord;
+
+		sRecord += Currency.Country() + delim;
+		sRecord += Currency.CurrencyCode() + delim;
+		sRecord += Currency.CurrencyName() + delim;
+		sRecord += to_string(Currency.Rate());
+
+		return sRecord;
+
+	}
+
+	static clsCurrency _ConvertCurrencyLineToRecordObject(string Line, string Seperator = "//##//") {
+
+		vector <string> vCurrency;
+		vCurrency = clsString::Split(Line, Seperator);
+
+		return clsCurrency(enMode::UpdateMode, vCurrency[0],vCurrency[1],vCurrency[2], stof(vCurrency[3]));
+	}
+
+	static vector <clsCurrency> _LoadFileContentCurrencyToVector() {
+		vector <clsCurrency> vCurrency;
+		fstream MyFile;
+
+		MyFile.open("Currencies.txt", ios::in);
+
+		if (MyFile.is_open()) {
+
+			string Line;
+
+			while (getline(MyFile, Line)) {
+
+
+				clsCurrency Currency = _ConvertCurrencyLineToRecordObject(Line);
+
+				vCurrency.push_back(Currency);
+
+			}
+
+			MyFile.close();
+		}
+		return vCurrency;
+	}
+
+	static vector <clsCurrency> _SaveDataCurrencyToFile(vector<clsCurrency>& vCurrency) {
+
+		fstream MyFile;
+
+		MyFile.open("Currencies.txt", ios::out);
+		string Line;
+		if (MyFile.is_open()) {
+
+
+
+			for (clsCurrency& C : vCurrency) {
+					Line = _ConvertCurrencyRecordToLine(C);
+					MyFile << Line << endl;
+			}
+
+			MyFile.close();
+		}
+		return vCurrency;
+	}
+
+	void _Update() {
+		vector <clsCurrency> _vUser;
+		_vUser = _LoadFileContentCurrencyToVector();
+
+		for (clsCurrency& C : _vUser) {
+			if (C.Country() == Country()) {
+
+				C = *this;
+				break;
+			}
+		}
+
+		_SaveDataCurrencyToFile(_vUser);
+
+
+	}
+
+public :
+	clsCurrency(enMode Mode,string Country, string CurrencyCode, string CurrencyName, float Rate) {
+
+		_Mode = Mode;
+		_Country = Country;
+		_CurrencyCode = CurrencyCode;
+		_CurrencyName = CurrencyName;
+		_Rate = Rate;
+
+	}
+
+	enMode IsEmpty() {
+		return (_Mode = enMode::EmpytMode);
+	}
+
+	string Country() {
+		return _Country;
+	}
+
+	string CurrencyCode() {
+		return _CurrencyCode;
+	}
+
+	string CurrencyName() {
+		return _CurrencyName;
+	}
+
+	 void UpdateRate(float Rate) {
+		_Rate = Rate;
+		_Update();
+	}
+
+	float Rate() {
+		return _Rate;
+	}
+
+	static clsCurrency FindCountry(string Country) {
+
+		Country = clsString::UpperAllLetters(Country);
+
+		fstream MyFile;
+
+		MyFile.open("Currencies.txt", ios::in);
+
+		if (MyFile.is_open()) {
+
+			string Line;
+
+			while (getline(MyFile, Line)) {
+
+				clsCurrency Currency = _ConvertCurrencyLineToRecordObject(Line);
+				if (clsString::UpperAllLetters(Currency.Country()) == Country) {
+					MyFile.close();
+					return Currency;
+				}
+
+
+			}
+
+
+		}
+		return _GetEmptyCurrencyObject();
+	}
+
+	static clsCurrency FindCurrencyCode(string CurrencyCode) {
+
+		CurrencyCode = clsString::UpperAllLetters(CurrencyCode);
+
+		fstream MyFile;
+
+		MyFile.open("Currencies.txt", ios::in);
+
+		if (MyFile.is_open()) {
+
+			string Line;
+
+			while (getline(MyFile, Line)) {
+
+				clsCurrency Currency = _ConvertCurrencyLineToRecordObject(Line);
+				if (Currency.CurrencyCode() == CurrencyCode) {
+					MyFile.close();
+					return Currency;
+				}
+
+
+			}
+
+
+		}
+		return _GetEmptyCurrencyObject();
+	}
+
+	static bool IsUserExist(string Country) {
+
+		clsCurrency Client = clsCurrency::FindCountry(Country);
+		return (!Client.IsEmpty());
+
+	}
+
+	static vector<clsCurrency> GetListCurrency() {
+		_LoadFileContentCurrencyToVector();
+	}
+
+};
+
